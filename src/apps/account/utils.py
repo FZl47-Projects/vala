f9a95053345cece9090839b994dcb2e5383cf18e
AmirMahdi Azarboon <amirazarboon@gmail.com>
2023-12-14 06:41:24 +0330

    Modify: account utils functions, Reset: migrations

--- src/apps/account/utils.py
+++ src/apps/account/utils.py
@@ -3,19 +3,31 @@ from django.core.validators import validate_email
 import re
 
 
+# Get coded phone number(IR)
+def get_coded_phone_number(number):
+    phone_number = str(number)
+    return '+98' + phone_number[1:]
+
+
+# Get raw phone number(IR)
+def get_raw_phone_number(number):
+    phone_number = str(number)
+    return '0' + phone_number[3:]
+
+
 # Check phone number format(IR)
-def check_phone_number(string):
+def check_phone_number(number):
     mobile_regex = "^09(1[0-9]|3[1-9]|2[1-9])-?[0-9]{3}-?[0-9]{4}$"
-    if re.search(mobile_regex, string):
+    if number and re.search(mobile_regex, number):
         return True
 
     return False
 
 
 # Check email format
-def check_email_format(string):
+def check_email_format(email):
     try:
-        validate_email(string)
+        validate_email(email)
         return True
     except ValidationError:
         return False
