7a051035d702997670a48805e3206fe1ae442157
AmirMahdi Azarboon <amirazarboon@gmail.com>
2023-12-16 00:28:23 +0330

    Add: index view/templates

--- src/apps/public/views.py
+++ src/apps/public/views.py
@@ -1,3 +1,11 @@
-from django.shortcuts import render
+from django.views.generic import TemplateView
+from django.utils.translation import gettext as _
+from django.shortcuts import redirect, reverse
 
-# Create your views here.
+
+# Render Index view
+class IndexView(TemplateView):
+    template_name = 'public/index.html'
+
+    def get_context_data(self, **kwargs):
+        pass
