a08fa2d7fbaf73e02076374bc3565bb85677d915
AmirMahdi Azarboon <amirazarboon@gmail.com>
2023-12-16 14:37:21 +0330

    Add: story ModelAdmin, Modify: add stories as context to index

--- src/apps/public/admin.py
+++ src/apps/public/admin.py
@@ -1,3 +1,10 @@
 from django.contrib import admin
 
-# Register your models here.
+from .models import Story
+
+
+# Register Story ModelAdmin
+@admin.register(Story)
+class StoryAdmin(admin.ModelAdmin):
+    list_display = ('title', 'image')
+    list_display_links = ('title',)
--- src/apps/public/views.py
+++ src/apps/public/views.py
@@ -2,10 +2,19 @@ from django.views.generic import TemplateView
 from django.utils.translation import gettext as _
 from django.shortcuts import redirect, reverse
 
+from .models import Story
+
 
 # Render Index view
 class IndexView(TemplateView):
     template_name = 'public/index.html'
 
     def get_context_data(self, **kwargs):
-        pass
+        context = super().get_context_data(**kwargs)
+        data = {
+            'stories': Story.get_recent_stories(),  # Recent 24 hours stories
+        }
+
+        context.update(data)
+
+        return context
