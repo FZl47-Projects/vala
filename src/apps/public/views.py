edd971adc9eaeda3a4faecd4198b70d00ab0325c
AmirMahdi Azarboon <amirazarboon@gmail.com>
2023-12-16 21:28:49 +0330

    Modify: send posts as context to index

--- src/apps/public/views.py
+++ src/apps/public/views.py
@@ -1,8 +1,9 @@
 from django.views.generic import TemplateView
 from django.utils.translation import gettext as _
 from django.shortcuts import redirect, reverse
+from django.db.models import Exists, OuterRef
 
-from .models import Story
+from .models import Story, Post, PostLike
 
 
 # Render Index view
@@ -13,6 +14,7 @@ class IndexView(TemplateView):
         context = super().get_context_data(**kwargs)
         data = {
             'stories': Story.get_recent_stories(),  # Recent 24 hours stories
+            'posts': Post.objects.annotate(user_liked=Exists(PostLike.objects.filter(post=OuterRef('pk'), user=self.request.user)))
         }
 
         context.update(data)
