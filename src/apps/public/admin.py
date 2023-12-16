625717b14bbef52cf864aeb5227e41c1ebd04ced
AmirMahdi Azarboon <amirazarboon@gmail.com>
2023-12-16 21:28:06 +0330

    Add: Post ModelAdmin

--- src/apps/public/admin.py
+++ src/apps/public/admin.py
@@ -1,6 +1,6 @@
 from django.contrib import admin
 
-from .models import Story
+from .models import Story, Post, PostComment
 
 
 # Register Story ModelAdmin
@@ -8,3 +8,18 @@ from .models import Story
 class StoryAdmin(admin.ModelAdmin):
     list_display = ('title', 'image')
     list_display_links = ('title',)
+
+
+# Register Post comments as inline
+class PostCommentInline(admin.StackedInline):
+    model = PostComment
+    extra = 0
+
+
+# Register Story ModelAdmin
+@admin.register(Post)
+class PostAdmin(admin.ModelAdmin):
+    list_display = ('title', 'category', 'image', 'is_active')
+    list_display_links = ('title',)
+    search_fields = ('title',)
+    inlines = (PostCommentInline,)
