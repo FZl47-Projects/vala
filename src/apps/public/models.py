25c2b1437b0002511f3e9cb0392b57081ecc8804
AmirMahdi Azarboon <amirazarboon@gmail.com>
2023-12-16 21:26:06 +0330

    Add: Post & PostLike & PostComment models

--- src/apps/public/models.py
+++ src/apps/public/models.py
@@ -4,6 +4,7 @@ from django.db import models
 from datetime import datetime, timedelta
 from apps.core.models import BaseModel
 from apps.core.utils import get_time
+from apps.account.models import User
 
 
 # Stories model
@@ -32,3 +33,53 @@ class Story(BaseModel):
         objects = cls.objects.filter(is_active=True, created_at__gte=active_time)
 
         return objects
+
+
+# Posts model
+class Post(BaseModel):
+    title = models.CharField(_('Title'), max_length=64, default=_('No title'))
+    caption = models.TextField(_('Caption'), null=True, blank=True)
+    category = models.CharField(_('Category'), max_length=64, null=True, blank=True)
+    image = models.ImageField(_('Image'), upload_to=f'images/post/{get_time("%Y-%m-%d")}/')
+    is_active = models.BooleanField(_('Active'), default=True)
+
+    class Meta:
+        verbose_name = _('Post')
+        verbose_name_plural = _('Posts')
+        ordering = ('-id',)
+
+    def __str__(self):
+        return self.title
+
+    def get_image_url(self):
+        if self.image:
+            return self.image.url
+
+
+# PostLikes model
+class PostLike(BaseModel):
+    user = models.ForeignKey(User, verbose_name=_('User'), on_delete=models.CASCADE, related_name='post_likes')
+    post = models.ForeignKey(Post, verbose_name=_('Post'), on_delete=models.CASCADE, related_name='post_likes')
+
+    class Meta:
+        verbose_name = _('Post like')
+        verbose_name_plural = _('Post likes')
+
+    def __str__(self):
+        return f'{self.user} - {self.post}'
+
+
+# PostComments model
+class PostComment(BaseModel):
+    user = models.ForeignKey(User, verbose_name=_('User'), on_delete=models.CASCADE, related_name='post_comments')
+    post = models.ForeignKey(Post, verbose_name=_('Post'), on_delete=models.CASCADE, related_name='post_comments')
+    text = models.TextField(_('Text'), max_length=255)
+    is_verified = models.BooleanField(_('Verified'), default=False)
+
+    class Meta:
+        verbose_name = _('Post comment')
+        verbose_name_plural = _('Post comments')
+        ordering = ('-id',)
+
+    def __str__(self):
+        return f'{self.user} - {self.post}'
