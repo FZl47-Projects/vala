c6147f839facb4b582385c47a0fb7f6b10c27cb2
AmirMahdi Azarboon <amirazarboon@gmail.com>
2023-12-28 21:47:17 +0330

    Add: AdminTicketsList view and answer method

--- src/apps/communication/urls.py
+++ src/apps/communication/urls.py
@@ -5,6 +5,7 @@ from . import views
 app_name = 'communication'
 
 urlpatterns = [
-    path('tickets/', views.TicketListView.as_view(), name='tickets_list'),
+    path('tickets/', views.TicketsListView.as_view(), name='tickets_list'),
     path('tickets/add/', views.AddTicketView.as_view(), name='add_ticket'),
+    path('tickets/all/', views.AdminTicketsListView.as_view(), name='all_tickets'),
 ]
--- src/apps/communication/views.py
+++ src/apps/communication/views.py
@@ -1,22 +1,50 @@
 from django.views.generic import TemplateView, CreateView, View
 from django.contrib.auth.mixins import LoginRequiredMixin
+from django.shortcuts import get_object_or_404, redirect
 from django.utils.translation import gettext as _
 from django.contrib.auth import get_user_model
 from django.urls import reverse_lazy
 from django.contrib import messages
 
+from apps.account.mixins import AccessRequiredMixin
 from .models import Ticket
 
 
 # Render TicketsListView
-class TicketListView(LoginRequiredMixin, TemplateView):
+class TicketsListView(LoginRequiredMixin, TemplateView):
     template_name = 'communication/tickets/ticket-list.html'
 
     def get_context_data(self, **kwargs):
         contexts = super().get_context_data(**kwargs)
-        contexts['tickets'] = Ticket.objects.filter(user=self.request.user, is_active=True)
+
+        tickets = Ticket.objects.filter(user=self.request.user, is_active=True)
+        contexts['tickets'] = tickets
 
         return contexts
+
+
+# Render AdminTicketsListView
+class AdminTicketsListView(AccessRequiredMixin, TemplateView):
+    template_name = 'communication/tickets/ticket-list-admin.html'
+    roles = ['admin']
+
+    def get_context_data(self, **kwargs):
+        contexts = super().get_context_data(**kwargs)
+
+        tickets = Ticket.objects.filter(is_active=True)
+        contexts['tickets'] = tickets
+
+        return contexts
+
+    def post(self, request):
+        data = request.POST.copy()
+        ticket = get_object_or_404(Ticket, id=data.get('id'))
+
+        ticket.answer = data.get('answer')
+        ticket.save()
+
+        messages.success(request, _('Answer successfully added'))
+        return redirect('communication:all_tickets')
 
 
 # Add Ticket view
