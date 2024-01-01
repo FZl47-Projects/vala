0faffb715e1de9892712ad91928e54ff5493f029
AmirMahdi Azarboon <amirazarboon@gmail.com>
2024-01-01 14:15:42 +0330

    Add: RecoveryProcess list/add/delete/details/ view and templates. Add menus url

--- src/apps/operation/urls.py
+++ src/apps/operation/urls.py
@@ -8,4 +8,11 @@ urlpatterns = [
     path('tests/', views.TestsView.as_view(), name='tests_list'),
     path('tests/<int:pk>/details/', views.TestDetailsView.as_view(), name='test_details'),
     path('tests/add/', views.AddTestView.as_view(), name='add_test'),
+
+    path('recovery/list/', views.RecoveryProcessesView.as_view(), name='recoveries_list'),
+    path('recovery/add/', views.AddRecoveryProcessView.as_view(), name='add_recovery_process'),
+    path('recovery/<int:pk>/delete/', views.DeleteRecoveryProcessView.as_view(), name='delete_recovery_process'),
+    path('recovery/<int:pk>/details/', views.RecoveryProcessDetailsView.as_view(), name='recovery_process_details'),
+    path('recovery/add-image/', views.AddRecoveryProcessImageView.as_view(), name='add_recovery_process_image'),
+    path('recovery/delete-image/', views.DeleteRecoveryProcessImageView.as_view(), name='delete_recovery_process_image'),
 ]
--- src/apps/operation/views.py
+++ src/apps/operation/views.py
@@ -1,11 +1,12 @@
 from django.views.generic import View, TemplateView, CreateView, DetailView
+from django.shortcuts import redirect, get_object_or_404, reverse
 from django.contrib.auth.mixins import LoginRequiredMixin
-from django.shortcuts import redirect, get_object_or_404
 from django.utils.translation import gettext as _
 from django.urls import reverse_lazy
 from django.contrib import messages
 
 from apps.account.mixins import AccessRequiredMixin
+from apps.notification.models import Notification
 from apps.account.models import User
 from . import models
 
@@ -40,6 +41,8 @@ class TestDetailsView(LoginRequiredMixin, DetailView):
         test.save()
 
         messages.success(request, _('The answer successfully registered'))
+
+        # TODO: Send sms for user to see the answer
         return redirect('operation:test_details', pk=test.pk)
 
 
@@ -64,3 +67,69 @@ class AddTestView(LoginRequiredMixin, CreateView):
         messages.success(self.request, _('Test added successfully'))
         return super().form_valid(form)
 
+
+# Render RecoveryProcesses view
+class RecoveryProcessesView(LoginRequiredMixin, TemplateView):
+    template_name = 'operation/recovery/processes-list.html'
+    
+    def get_context_data(self, **kwargs):
+        contexts = super().get_context_data(**kwargs)
+        processes = models.RecoveryProcess.objects.filter(is_active=True, user=self.request.user)
+        contexts['processes'] = processes
+
+        return contexts
+
+
+# Add RecoveryProcess view
+class AddRecoveryProcessView(LoginRequiredMixin, CreateView):
+    template_name = 'operation/recovery/processes-list.html'
+    success_url = reverse_lazy('operation:recoveries_list')
+    model = models.RecoveryProcess
+    fields = ('user', 'title')
+    
+    def form_valid(self, form):
+        messages.success(self.request, _('Recovery process added successfully'))
+        return super().form_valid(form)
+
+
+# Delete RecoveryProcess view
+class DeleteRecoveryProcessView(LoginRequiredMixin, View):
+
+    def get(self, request, pk):
+        obj = get_object_or_404(models.RecoveryProcess, pk=pk)
+        obj.is_active = False
+        obj.save()
+
+        messages.success(request, _('Recovery process deleted successfully'))
+        return redirect('operation:recoveries_list')
+
+
+# Render RecoveryProcessDetails view
+class RecoveryProcessDetailsView(LoginRequiredMixin, DetailView):
+    template_name = 'operation/recovery/process-details.html'
+    model = models.RecoveryProcess
+
+
+# Add RecoveryProcessImage view
+class AddRecoveryProcessImageView(LoginRequiredMixin, CreateView):
+    template_name = 'operation/recovery/process-details.html'
+    model = models.RecoveryProcessImage
+    fields = ('recovery_process', 'image')
+
+    def form_valid(self, form):
+        messages.success(self.request, _('Image added successfully'))
+        return super().form_valid(form)
+
+
+# Delete RecoveryProcessImage view
+class DeleteRecoveryProcessImageView(LoginRequiredMixin, View):
+
+    def post(self, request):
+        data = request.POST.copy()
+        obj = get_object_or_404(models.RecoveryProcessImage, pk=data.get('pk'))
+
+        parent_pk = obj.recovery_process.pk
+        obj.delete()
+
+        messages.success(self.request, _('Image deleted successfully'))
+        return redirect('operation:recovery_process_details', parent_pk)
