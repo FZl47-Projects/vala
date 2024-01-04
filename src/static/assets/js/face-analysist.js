110d1fc3af36a829ef3ba98973f3ebc9094c9eac
AmirMahdi Azarboon <amirazarboon@gmail.com>
2024-01-05 01:15:31 +0330

    Modify: static files(js/style/image)

--- src/static/assets/js/face-analysist.js
+++ src/static/assets/js/face-analysist.js
@@ -1,70 +1,89 @@
 
-import { analyze } from "./api/analyze.js";
-import { getDataLocal } from "./helper.js";
-const userId = await getDataLocal("user");
+const steps_one = document.querySelectorAll(".step-1");
+const steps_two = document.querySelectorAll(".step-2");
+const steps_three = document.querySelectorAll(".step-3");
+const steps_four = document.querySelectorAll(".step-4");
+const content_one = document.querySelector("#upload-step-1");
+const content_two = document.querySelector("#upload-step-2");
+const content_three = document.querySelector("#upload-step-3");
+const content_four = document.querySelector("#upload-step-4");
 
+steps_one.forEach((item, index) => {
+    item.addEventListener("click", () => {
+        content_one.classList.add("active");
+        content_two.classList.remove("active");
+        content_three.classList.remove("active");
+        content_four.classList.remove("active");
+    })
+});
 
-const PictureOne = document.querySelector("#picture-post-add-one");
-const PictureTwO = document.querySelector("#picture-post-add-two"); 
-const PictureTtee = document.querySelector("#picture-post-add-tree");
-const SendBtn = document.querySelector("#finish");
-SendBtn.addEventListener("click", async () => {
-  var formdata = new FormData();
-  formdata.append("image1", PictureOne.files[0], PictureOne.value);
-  formdata.append("image2", PictureTwO.files[0], PictureTwO.value);
-  formdata.append("image3", PictureTtee.files[0], PictureTtee.value);
-  formdata.append("user", userId);
+steps_two.forEach((item, index) => {
+    item.addEventListener("click", () => {
+        if ($('#upload-step-1 input[type=file]')[0].files.length === 0) {
+            Toast.fire({
+                icon: 'error',
+                title: 'لطفا تصویر را آپلود کنید',
+                timer: 3000
+            });
+            return
+        }
 
+        content_one.classList.remove("active");
+        content_two.classList.add("active");
+        content_three.classList.remove("active");
+        content_four.classList.remove("active");
+    })
+});
 
-  
-  var requestOptions = {
-    method: "POST",
-    body: formdata,
-    redirect: "follow",
-  };
-  await analyze(requestOptions);
+steps_three.forEach((item, index) => {
+    item.addEventListener("click", () => {
+        if ($('#upload-step-2 input[type=file]')[0].files.length === 0) {
+            Toast.fire({
+                icon: 'error',
+                title: 'لطفا تصویر را آپلود کنید',
+                timer: 3000
+            });
+            return
+        }
+
+        content_one.classList.remove("active");
+        content_two.classList.remove("active");
+        content_three.classList.add("active");
+        content_four.classList.remove("active");
+    })
+});
+
+steps_four.forEach((item, index) => {
+    item.addEventListener("click", () => {
+        if ($('#upload-step-3 input[type=file]')[0].files.length === 0) {
+            Toast.fire({
+                icon: 'error',
+                title: 'لطفا تصویر را آپلود کنید',
+                timer: 3000
+            });
+            return;
+        }
+
+        content_one.classList.remove("active");
+        content_two.classList.remove("active");
+        content_three.classList.remove("active");
+        content_four.classList.add("active");
+    })
 });
 
 
+// ------------------------------ Set image on change input -----------------------------
+function displayImage(event) {
+    let fileInput = event.target;
+    let file = fileInput.files[0];
 
+    if (file) {
+        let reader = new FileReader();
+        reader.onload = function (e) {
+            let selectedImage = document.getElementById(fileInput.name);
+            selectedImage.src = e.target.result;
+        };
 
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-const step_two = document.querySelector("#step-2");
-const step_tree = document.querySelector("#step-3");
-const content_one = document.querySelector("#upload-step-1");
-const content_two = document.querySelector("#upload-step-2");
-const content_tree = document.querySelector("#upload-step-3");
-step_two.addEventListener("click", () => {
-    content_two.classList.add("active");
-    content_one.classList.remove("active");
-
-  });
-
-  step_tree.addEventListener("click", () => {
-    content_tree.classList.add("active");
-    content_two.classList.remove("active");
-  });
-
-
-
+        reader.readAsDataURL(file);
+    }
+}
